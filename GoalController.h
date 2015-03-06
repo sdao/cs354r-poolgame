aa5f69cd5d428f8f0fad947bc34d57dfcd8755c0
Josan Munoz <jmunoz80@cs.utexas.edu>
2015-03-06 15:17:11 -0600

    merged and pushing negative score when hitting pockets

--- GoalController.h
+++ GoalController.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "Component.h"
+#include "GameInfo.h"
 
 class GoalController : public Component {
+	std::shared_ptr<GameInfo> gameinfo;
+
 public:
+  GoalController(std::shared_ptr<GameInfo>);
   virtual void didCollide(PhysicsCollider& collider) override;
 };
 
